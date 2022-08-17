a6d779bb86391975acff8e03e774bb950d23594c
Dogpa <dogpa0130@gmail.com>
2022-08-17 22:09:03 +0800

    Initial Commit

---
+++ Swift Practice 170 MapSpanStudy/Swift_Practice_170_MapSpanStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_170_MapSpanStudyApp.swift
+//  Swift Practice 170 MapSpanStudy
+//
+//  Created by Dogpa's MBAir M1 on 2022/8/17.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_170_MapSpanStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
